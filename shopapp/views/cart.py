9e7d056f3f41d8f49e33ea59ffa2815453d0ff99
gkswngh05 <49218038+gkswngh05@users.noreply.github.com>
2023-11-23 16:10:43 +0900

    .

--- shopapp/views/cart.py
+++ shopapp/views/cart.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 
+from ..models import CartList
+
 @login_required
 def cart(request):
+    # user = request.user
+    # items = user.cartlist_set.filter().order_by()
     return render(request, 'cart.html')
