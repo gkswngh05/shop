70b8e67b2a285161565ecae5aafb620c0fbd1ed3
gkswngh05 <49218038+gkswngh05@users.noreply.github.com>
2023-11-23 21:54:01 +0900

    카트 중간 커밋

--- shopapp/views/cart.py
+++ shopapp/views/cart.py
@@ -6,6 +6,24 @@ from ..models import CartList
 
 @login_required
 def cart(request):
-    # user = request.user
-    # items = user.cartlist_set.filter().order_by()
-    return render(request, 'cart.html')
+    user = request.user
+    cart = user.cart.cartlist_set.all()
+    items = []
+    cost = {
+        "grandDeliveryCost" : 0,
+        "grandPrice" : 0,
+    }
+    for i in cart:
+        items.append({
+            "name" : i.item.name,
+            "amount" : i.amount,
+            "seller" : i.item.sellerid.name,
+            "deliveryCost" : i.item.deliveryCost,
+            "price" : i.item.price,
+            "totalPrice" : i.item.price * i.amount,
+            })
+        cost["grandDeliveryCost"] += i.item.deliveryCost
+        cost["grandPrice"] += i.item.price * i.amount
+    
+    print(items)
+    return render(request, 'cart.html', {"items" : items, "cost" : cost})
