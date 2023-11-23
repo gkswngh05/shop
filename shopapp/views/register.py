f14639de7326293a3a6c45b1e04150406d4b31b1
gkswngh05 <49218038+gkswngh05@users.noreply.github.com>
2023-11-23 13:48:51 +0900

    회원가입 초기 구현

--- shopapp/views/register.py
+++ shopapp/views/register.py
@@ -1,5 +1,45 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
+from django.http import HttpResponse
+
+from django import forms
+
+from ..models import myUser
+
+class RegisterForm(forms.ModelForm):
+    
+    password1 = forms.CharField(label="Password", widget=forms.PasswordInput)
+    password2 = forms.CharField(
+        label="Password confirmation", widget=forms.PasswordInput
+    )
+    
+    class Meta:
+        model = myUser
+        fields = ["userName", "name", "callNumber", "address", "is_seller"]
+    
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match")
+        return password2
+    
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password2"])
+        if commit:
+            user.save()
+        return user
+    
 
 def register(request):
-    return render(request, 'register.html')
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            #return HttpResponse("Valid")
+            form.save()
+            return redirect("shopapp:index")
+        else :
+            return HttpResponse("Invalid")
+    else: form = RegisterForm()
+    return render(request, 'register.html', {"form" : form})
