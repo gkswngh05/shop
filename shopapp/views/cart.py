9daa8ee1cc96cc5d549f3e773d9de528d244fff1
gkswngh05 <49218038+gkswngh05@users.noreply.github.com>
2023-11-23 22:26:54 +0900

    장바구니 추가 삭제 초기 구현

--- shopapp/views/cart.py
+++ shopapp/views/cart.py
@@ -2,11 +2,22 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 
-from ..models import CartList
+from ..models import Item
 
 @login_required
 def cart(request):
     user = request.user
+        
+    if request.method == "POST":
+        item = request.POST.get("item")
+        amount = request.POST.get("amount")
+        if request.POST.get("operation") == '0':
+            user.cart.cartlist_set.create(item = Item.objects.get(id = int(item)), amount = int(amount))
+        elif request.POST.get("operation") == '1':
+            user.cart.cartlist_set.get(item = Item.objects.get(id = int(item))).delete()
+        else:
+            return HttpResponse("올바르지 않은 요청입니다.")
+    
     cart = user.cart.cartlist_set.all()
     items = []
     cost = {
@@ -15,6 +26,7 @@ def cart(request):
     }
     for i in cart:
         items.append({
+            "id" : i.item.id,
             "name" : i.item.name,
             "amount" : i.amount,
             "seller" : i.item.sellerid.name,
